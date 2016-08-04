5badaba513da3552972d43294b8f63b45a31ad2c
jjungle <jung@clarat.org>
2016-08-04 22:29:20 +0200

    added username to devise registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   get '/songs/new' => 'songs#new'
   post '/songs' => 'songs#create'
   get '/songs/thanks' => 'songs#thanks', as: :songs_thanks
@@ -11,6 +12,8 @@ Rails.application.routes.draw do
 
   get '/rounds/new' => 'rounds#new'
   post '/rounds' => 'rounds#create'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
