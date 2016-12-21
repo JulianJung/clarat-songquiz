fb9795a1f1e2ea155a1a8af8c7e88d88678f451d
Konstantin Koss <konstantin.koss@gmail.com>
2016-12-21 17:05:46 +0100

    TODO Kommentar für #4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+  
+  # TODO: private Funktion, die sagt, ob man sich gerade am richtigen Ort befindet.
 end
